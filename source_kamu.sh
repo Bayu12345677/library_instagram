0477e98fbf6a852cbdf6844e4efd8e11c736ccc0
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2022-01-01 01:25:51 +0700

    Create source_kamu.sh

---
+++ source_kamu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source "$(pwd)"/lib/moduler.sh
+
+Bash.import "$(pwd)/lib/text_display/string.sh"
+Bash.import "$(pwd)/lib/instagram-dl/ig-dl.sh"
+
+# cara memakai ig-dl : Bash::mode.dl.ig url nama
+
+Tulis.strN "halo world"
